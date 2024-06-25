5aefa9bd96e1e7f34620506427f7f75540c05879
José Alfredo Arroyo Gómez <arroyo.alfredo.1pm@gmail.com>
2024-06-25 17:59:42 -0600

    265. Mostrando los mensajes de error en la validación

---
+++ src/views/LoginView.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { useForm, useField } from 'vee-validate';
+import { loginSchema as validationSchema } from '../validation/loginSchema'
+
+const { handleSubmit } = useForm({
+    validationSchema,
+});
+
+const email = useField('email');
+const password = useField('password');
+
+const submit = handleSubmit(() => {
+    console.log('submit');
+});
+
+</script>
+
+<template>
+    <v-card flat max-width="600" class="mx-auto my-10">
+        <v-card-title class="text-h4 font-weight-bold" tag="h4">
+            Iniciar Sesión
+        </v-card-title>
+
+        <v-card-subtitle class="text-h5" tag="h5">
+            Inicia Sesión con tu cuenta
+        </v-card-subtitle>
+
+        <v-form class="mt-5">
+            <v-text-field 
+            class="mb-3"
+            type="email" label="Email" bg-color="blue-grey-lighten-5" v-model="email.value.value"
+                :error-messages="email.errorMessage.value" />
+
+            <v-text-field 
+            class="mb-3"
+            type="password" label="Password" bg-color="blue-grey-lighten-5" v-model="password.value.value"
+                :error-messages="password.errorMessage.value" />
+
+            <v-btn block color="pink-accent-3" @click="submit">
+                Iniciar Sesión
+            </v-btn>
+        </v-form>
+
+    </v-card>
+</template>
